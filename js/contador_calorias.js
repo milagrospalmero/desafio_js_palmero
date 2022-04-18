656b9fbd259ff5005632077fed6675abe6504015
milagros palmero <milagrospalmero@hotmail.com>
2022-04-18 11:20:15 -0300

    desafio

---
+++ js/contador_calorias.js
@@ -0,0 +1,21 @@
+let maximo_calorias = parseInt(prompt("Ingrese la Cantidad Máxima de Calorías"));
+let suma_calorias = 0;
+
+
+
+for (i=0; i<6; i++) {
+    let comida = prompt("Ingrese el plato que vas a comer:");
+    let calorias = parseInt(prompt("Ingrese la cantidad de calorías que contiene: " + comida));
+    suma_calorias += calorias;
+
+    if (suma_calorias > maximo_calorias) {
+        alert("Superaste el consumo máximo de calorías!");
+        break;
+        } else { 
+            let restan_calorias = maximo_calorias - calorias ;
+            alert("Te quedan " + restan_calorias + " calorías por consumir" );
+            
+        breack;  
+        }
+    }    
+        alert("Consumiste: " + suma_calorias + " calorías!"); 
